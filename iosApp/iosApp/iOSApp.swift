38091a0034f23a6f489dfbb71c980d248e187ef2
gowtham <gowthamon@facilio.com>
2025-06-15 01:34:11 +0530

    pod files

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,11 +1,15 @@
 import SwiftUI
 import ComposeApp
+import GoogleMaps
 
 @main
 struct iOSApp: App {
+
     init() {
         KoinKt.doInitKoin()
+        GMSService.provideAPIKey("MAP_API_KEY")
     }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
